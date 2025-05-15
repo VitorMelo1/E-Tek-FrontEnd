d161eb143de0fa71e4731fd0ec83f6c2d9ad9959
Vitor Martins Melo <seuemail@email.com>
2025-05-15 14:49:59 -0300

    feat: atualiza IntroSection.tsx com melhorias na interface e correções de bugs

--- FrontEnd/src/componets/intro/IntroSection.tsx
+++ FrontEnd/src/componets/intro/IntroSection.tsx
@@ -7,16 +7,16 @@ const IntroSection: React.FC = () => {
     <section className={`${styles.intro} ${styles.card}`}>
       <div className={styles.textContent}>
         <p>
-          Não é só tecnologia.<br />
-          É atitude em forma de produto.<br />
-          (E-TEK = personalidade + potência).
+          It’s not just technology.<br />
+          It’s attitude turned into product.<br />
+          (E-TEK = personality + power).
         </p>
       </div>
       <div className={styles.buttonContainer}>
-        <button className={styles.button}>Ver Produtos</button>
+        <button className={styles.button}>View Products</button>
       </div>
       <div className={styles.logoWrapper}>
-        <img src={EtekLogo} alt="Logo E-TEK" className={styles.logoGlow} />
+        <img src={EtekLogo} alt="E-TEK Logo" className={styles.logoGlow} />
       </div>
     </section>
   );
